d40ebfdfeec78bb9240036f3ffcc28891e322fdb
marc48 <mluder@dimatools.ch>
2017-11-29 15:54:29 +0100

    Forms 2.5.0.91635, DryIoc 2.12.5

--- PrismDemoD/PrismDemoD/ViewModels/BookPageViewModel.cs
+++ PrismDemoD/PrismDemoD/ViewModels/BookPageViewModel.cs
@@ -1,14 +1,18 @@
 ﻿using Prism.Navigation;
-using Prism.Mvvm;
+using Prism.Commands;
 using PrismDemoD.Models;
+using Prism.Services;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using Xamarin.Forms;
 
 namespace PrismDemoD.ViewModels
 {
     public class BookPageViewModel : ViewModelBase
     {
+        private IPageDialogService _dialogService;
+
         private Book _book;
         public Book Book
         {
@@ -16,9 +20,18 @@ namespace PrismDemoD.ViewModels
             set { SetProperty(ref _book, value); }
         }
 
-        public BookPageViewModel()
-        {
+        public DelegateCommand ReadBookCommand { get; set; }
 
+        public BookPageViewModel(IPageDialogService dialogService)
+        {
+            _dialogService = dialogService;
+            ReadBookCommand = new DelegateCommand(showAlert);
+        }
+
+        private async void showAlert()
+        {
+            // show alert with response
+            var answer = await _dialogService.DisplayAlertAsync("Prism: DisplayAlertAsync", "Did you read this book?", "Yes", "No");    
         }
 
         public override void OnNavigatingTo(NavigationParameters parameters)
